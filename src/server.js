b57375a3352e1dc4ece94c73b76e129baa7e5f6c
htbacgiang <truongtl27.ht@gmail.com>
2022-05-29 00:04:08 +0700

    as

--- src/server.js
+++ src/server.js
@@ -6,9 +6,10 @@ const initWebRoutes = require("./route/web.js");
 const connectDB = require("./config/connectDB.js");
 require('dotenv').config();
 
+let app = express();
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://truongnq.vn');
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
